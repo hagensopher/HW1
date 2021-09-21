9ee727946b524dc47f95a2b0df9e7e5114c30461
Hagen <hagen-sopher@tamu.edu>
2021-09-20 19:00:11 -0500

    making main work

--- Q2/Main.java
+++ Q2/Main.java
@@ -6,9 +6,19 @@ public class Main{
 
     public static void main(String[] args){
         Shape shape[] = new Shape[args.length];
+       
+        for(int i=0;i<shape.length;i++){
+            if(shape[i].equals(Rectangle)){
+                
+            }
+            else if(shape[i].equals(Circle)){
+
+            }
+            else { //Triangle
+
+            }
+        }
         
-        
-        
-        System.out.printf("The total area for the %d objects is 1.2%f units squared. \n",shape.length, AreaCalculator.calculate(shape));
+      //  System.out.printf("The total area for the %d objects is 1.2%f units squared. \n",shape.length, AreaCalculator.calculate(shape));
     }
 }
